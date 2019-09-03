e7e8b33c5d5b0b538df36caed4558108e9bef6d1
ASPP Student <student@localhost>
2019-09-03 15:10:16 +0200

    deleted superfluous code

--- hands_on/numerical_fuzzing/numerical_fuzzing/test_var.py
+++ hands_on/numerical_fuzzing/numerical_fuzzing/test_var.py
@@ -3,7 +3,7 @@ from math import isclose
 
 def test_var_deterministic():
     x = np.array([0.0, 1.0, 2.0, 3.0])
-    print(np.var(x))
+
     expected = 1.25
     assert isclose(np.var(x), expected)
 
@@ -13,9 +13,9 @@ def test_var_fuzzing():
 
     N, D = 100000, 5
     # Goal means: [0.1 ,  0.45,  0.8 ,  1.15,  1.5]
-    testdata = np.linspace(0.1, 1.5, D)
+
     expected = np.ones(D)
     # Generate random, D-dimensional data with the desired mean
-    x = rand_state.randn(N, D) + testdata
+    x = rand_state.randn(N, D)
     vars = np.var(x, axis=0)
     np.testing.assert_allclose(vars, expected, rtol=1e-2)
