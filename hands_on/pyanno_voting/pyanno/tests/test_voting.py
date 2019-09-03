30522c5b9f6b5b491cebf40016d93ace744da519
ASPP Student <student@localhost>
2019-09-03 12:29:08 +0200

    refactored test_labels_frequency and test_labels_count

--- hands_on/pyanno_voting/pyanno/tests/test_voting.py
+++ hands_on/pyanno_voting/pyanno/tests/test_voting.py
@@ -7,6 +7,7 @@ from math import isclose
 
 
 def test_labels_count():
+    #given
     annotations = [
         [1,  2, MV, MV],
         [MV, MV,  3,  3],
@@ -15,8 +16,47 @@ def test_labels_count():
     ]
     nclasses = 5
     expected = [0, 3, 1, 3, 0]
+
+    #when
     result = voting.labels_count(annotations, nclasses)
+
+    #then
     assert result == expected
+
+
+def test_labels_frequency():
+    #given
+    matrix = [
+        [1, 2, 2, -1],
+        [2, 2, 2, 2],
+        [1, 1, 3, 3],
+        [1, 3, 3, 2],
+        [-1, 2, 3, 1],
+        [-1, -1, -1, 3],
+    ]
+
+    matrix2 = [
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1]
+    ]
+
+    lowerlimit = 0
+    upperlimit = 1
+    nclasses = 4
+    
+    expected2 = np.zeros(nclasses)
+
+
+    #when
+    result = voting.labels_frequency(matrix, nclasses)
+    result2 = voting.labels_frequency(matrix2, nclasses)
+
+    #then
+    assert np.all([res != None for res in result])
+    assert len(result) == nclasses
+    assert np.all(result2 == expected2)
+    assert np.all([i >= lowerlimit and i <= upperlimit for i in result])
+    assert isclose(np.sum(result),upperlimit) or isclose(np.sum(result), lowerlimit,abs_tol=1e-12)
 
 
 def test_majority_vote():
@@ -43,20 +83,3 @@ def test_majority_vote_empty_item():
     expected = [1, MV, 2]
     result = voting.majority_vote(annotations)
     assert result == expected
-
-def test_labels_frequency():
-    matrix = [
-        [1, 2, 2, -1],
-        [2, 2, 2, 2],
-        [1, 1, 3, 3],
-        [1, 3, 3, 2],
-        [-1, 2, 3, 1],
-        [-1, -1, -1, 3],
-    ]
-    result = voting.labels_frequency(matrix, 4)
-
-    assert np.all([res != None for res in result])
-    assert len(result) == 4
-    assert np.all(voting.labels_frequency([[-1, -1, -1, -1],[-1, -1, -1, -1]], 4) == np.zeros(4))
-    assert np.all([i >= 0 and i <= 1 for i in result])
-    assert isclose(np.sum(result),1) or isclose(np.sum(result), 0,abs_tol=1e-12)
